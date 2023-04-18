09fd3ed7ae34cc9d59f443a5e4476640519e40f2
yun seok <str17gogo@naver.com>
2023-04-18 15:34:28 +0900

    first commit

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class Main {
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello();
+        printHello.print();
+
+        PrintBye printBye = new PrintBye();
+        printBye.printBye();
+    }
+}
---
+++ src/main/java/com/example/demo/PrintBye.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class PrintBye {
+    public void printBye() {
+        System.out.println("Bye");
+    }
+}
